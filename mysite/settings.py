9e3fbeb3ecb6094a36c74cd801daff4991816c7f
ilyavred <artcore02@gmail.com>
2020-03-26 00:01:52 +0600

    Add comments adopt for heroku runtime -py 3.7.6

--- mysite/settings.py
+++ mysite/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'djangogirls',
+        'NAME': 'sitedata',
         'USER': 'name',
         'PASSWORD': '',
         'HOST': 'localhost',
