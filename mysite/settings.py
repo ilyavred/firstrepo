aed7defbd6480e8041aa63b8d5a2ddea7bd87854
ilyavred <artcore02@gmail.com>
2020-03-26 01:12:42 +0600

    DISABLE_COLLECTSTATIC

--- mysite/settings.py
+++ mysite/settings.py
@@ -137,6 +137,7 @@ STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/'
 
+DISABLE_COLLECTSTATIC = 1
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
